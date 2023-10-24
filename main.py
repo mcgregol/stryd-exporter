b6c8ba8b11c1c44d0313e874d6dbc3e9eb494950
Liam McGregor <liam@pop-os.localdomain>
2023-10-24 17:45:14 -0400

    modified:   main.py modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from exporter import Exporter
+from appJar import gui
+import asyncio, time, schedule
+
+#  start gui
+app = gui("Stryd Exporter by ACHIEVE", "800x500")
+app.setBg("grey")
+app.go()
+
+
+
+
+
+
+
+
 ## TODO
 ##  every instance of exporter runs on async timer
 ##  create "kill" function
