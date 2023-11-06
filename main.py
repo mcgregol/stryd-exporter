706a2322885e4f539627cabf8d52ed5955227b4f
Liam McGregor <liam@pop-os.localdomain>
2023-11-06 14:57:12 -0500

    modified:   main.py new file:   team.py

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from exporter import Exporter
-from appJar import gui
+from tkinter import *
+from tkinter import ttk
 import asyncio, time, schedule
 
+def new_team():
+	pass
+
+def kill_team():
+	pass
+
+def edit_team():
+	pass
+
+def pause_team():
+	pass
+
+#  create window/frame
+root = Tk()
+root.title("Stryd Exporter")
+root.geometry("800x500")
+
+#  title
+ttk.Label(text="Teams:").place(x=90, y=50)
+
+#  create listbox for teams
+listbox = Listbox()
+listbox.place(x=40, y=80)
+
+#  button to create team
+ttk.Button(text="New", command=new_team).pack(side="right", padx=300)
+
 #  start gui
-app = gui("Stryd Exporter by ACHIEVE", "800x500")
-app.setBg("grey")
-app.go()
-
-
-
-
+root.mainloop()
 
 
 
---
+++ team.py
@@ -0,0 +1,3 @@
+class Team:
+	def __init__(self, exporter_list):
+		self.exporter_list = [exporter_list]
