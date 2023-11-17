4fef64d9f6ca541ad11037a616c17da86537617f
Steve McGregor <achieve@Steves-MacBook-Pro.local>
2023-11-16 21:38:13 -0500

    new file:   setup.bat

---
+++ v1/setup.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+echo Welcome to setup.
+pause
+
+echo Installing dependencies...
+pip install -r ../requirements.txt
+
+echo Done!
+echo run 'python october.py' or 'python november.py'
